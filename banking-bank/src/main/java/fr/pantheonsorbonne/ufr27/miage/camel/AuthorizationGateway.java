e53fef539113f1cf99fdf7d7c16fafa6631269ba
krishnath92 <krish92130@gmail.com>
2024-01-14 15:16:22 +0100

    add AuthorizationGateway

--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/AuthorizationGateway.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/AuthorizationGateway.java
@@ -22,7 +22,7 @@ public class AuthorizationGateway {
         try (ProducerTemplate producerTemplate = context.createProducerTemplate()) {
             if (response.equals("Yes")) {
                 String token = tokenService.generateToken(user.getEmail());
-                producerTemplate.sendBodyAndHeader("direct:receiveToken", token, "bankName", bankName);
+                producerTemplate.sendBodyAndHeader("direct:sendToken", token, "bankName", bankName);
             }
         }
         catch (TokenGenerationException e) {
